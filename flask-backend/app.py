1248f348239430b176efaeaa46a0c0db40b0c4c5
YazanGhuniem <158981924+YazanGhuniem@users.noreply.github.com>
2025-02-17 03:18:41 -0500

    Added /init-db route to initialize database

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -49,6 +49,12 @@ def delete_task(task_id):
         flash('Task deleted!', 'danger')
     return redirect(url_for('index'))
 
+@app.route('/init-db')
+def init_db():
+    with app.app_context():
+        db.create_all()
+    return "✅ Database initialized!"
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  
